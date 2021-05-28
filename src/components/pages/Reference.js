e0d79694c1d10475c9cb0624d39d6375ebd3f982
kim-yejin-3955 <80299427+kim-yejin-3955@users.noreply.github.com>
2021-05-28 17:17:50 +0900

    2021.05.28/2

--- src/components/pages/Reference.js
+++ src/components/pages/Reference.js
@@ -1,4 +1,6 @@
+import axios from "axios";
 import React from "react";
+import Loading from "../basics/Loading";
 import Header from "../Header";
 import Layout from "../Layout";
 // import Footer from "../Footer";
@@ -36,48 +38,88 @@ import WrapTitle from "../basics/WrapTitle";
 
 // function Reference() {
 //   return (
-//     <div id="wrap" className="light">
-//       <Header info="none" />
-//       <Layout>
-//         <section id="referCont">
-//           <div className="container">
-//             <WrapTitle text={["HTML", "reference"]} />
-//             <div className="refer-cont">
-//               <div className="refer-table">
-//               <h2>CSS REFERENCE </h2>
-//                 <ul>
-//                   {referAttr.map((el) => (
-//                       <ReferText
-//                       key={el.attr}
-//                       alpha={el.alpha}
-//                       attr={el.attr}
-//                       desc={el.desc}
-//                       />
-//                   )) }
-//                 </ul>
-//               </div>
+//   <div id="wrap" className="light">
+//     <Header info="none" />
+//     <Layout>
+//       <section id="referCont">
+//         <div className="container">
+//           <WrapTitle text={["HTML", "reference"]} />
+//           <div className="refer-cont">
+//             <div className="refer-table">
+//             <h2>CSS REFERENCE </h2>
+//               <ul>
+//                 {referAttr.map((el) => (
+//                     <ReferText
+//                     key={el.attr}
+//                     alpha={el.alpha}
+//                     attr={el.attr}
+//                     desc={el.desc}
+//                     />
+//                 )) }
+//               </ul>
 //             </div>
 //           </div>
-//         </section>
-//       </Layout>
-//     </div>
-//   );
+//         </div>
+//       </section>
+//     </Layout>
+//   </div>
+// );
 // }
 
 class Reference extends React.Component {
   state = {
     isLoading: true,
+    refers: [],
   };
 
-  componentDidMount(){
-      setTimeout(()=>{
-          this.setState({isLoading:false})
-      },3000)
+  
+
+  getRefer = async () => {
+    const {
+      data: {
+        data: { htmlRefer },
+      },
+    } = await axios.get(
+      // "https://kim-yejin-3955.github.io/react4000/src/json/reference.json"
+      "https://webstoryboy.github.io/react3001/src/json/reference.json"
+    );
+    //console.log(htmlRefer);
+    this.setState({ htmlRefer, isLoading: false });
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      // this.setState({isLoading:false})
+      this.getRefer();
+    }, 3000);
   }
 
   render() {
     const { isLoading } = this.state;
-    return <div>{isLoading ? "로딩중" : "준비되었습니다"}</div>;
+    return (
+      <div>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <div id="wrap" className="light">
+            <Header info="none" />
+            <Layout>
+              <section id="referCont">
+                <div className="container">
+                  <WrapTitle text={["HTML", "reference"]} />
+                  <div className="refer-cont">
+                    <div className="refer-table">
+                      <h2>CSS REFERENCE </h2>
+                      <ul></ul>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </Layout>
+          </div>
+        )}
+      </div>
+    );
   }
 }
 
